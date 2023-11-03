f234dabb6a723b965d29b25bcd7193af137a3707
RoSeiler <seilerrocio@gmail.com>
2023-11-02 21:24:38 -0300

    adding some typescript practice from FrontendMasters

---
+++ name-badges/src/components/application.tsx
@@ -0,0 +1,9 @@
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge name={'Ro Seiler'} />
+  </main>
+);
+
+export default Application;
